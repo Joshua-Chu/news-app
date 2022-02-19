7ac8ec54fb37223a049b6af0b74e705ad4fcd41e
Joshua-Chu <jpchu@up.edu.ph>
2022-02-19 16:24:19 +0800

    fix: nav link abstraction

--- component/Navbar/Navbar.tsx
+++ component/Navbar/Navbar.tsx
@@ -6,8 +6,55 @@ import {
     Stack,
     useBreakpointValue,
 } from "@chakra-ui/react";
-import Link from "next/link";
+import NextLink from "next/link";
+import React from "react";
 import { Logo } from "../Logo";
+
+type ButtonProps = {
+    currPath: string;
+    children: React.ReactNode;
+    href: string;
+};
+
+const GhostLink = ({ currPath, children, href }: ButtonProps) => {
+    return (
+        <NextLink href={href} passHref>
+            <Button
+                size="sm"
+                colorScheme="gray"
+                variant="ghost"
+                as="a"
+                bg={currPath === "/login" ? "gray.100" : ""}
+            >
+                <Heading as="p" fontSize="sm">
+                    {children}
+                </Heading>
+            </Button>
+        </NextLink>
+    );
+};
+
+const SolidLink = ({ currPath, children, href }: ButtonProps) => {
+    return (
+        <NextLink href={href} passHref>
+            <Button
+                size="sm"
+                bg={currPath === "/signup" ? "gray.100" : "gray.500"}
+                variant="solid"
+                as="a"
+                _hover={{
+                    color: currPath === "/signup" ? "white" : "gray.600",
+                    bg: currPath === "/signup" ? "gray.500" : "gray.100",
+                }}
+                color={currPath === "/signup" ? "gray.600" : "white"}
+            >
+                <Heading as="p" fontSize="sm">
+                    {children}
+                </Heading>
+            </Button>
+        </NextLink>
+    );
+};
 
 type NavbarProps = {
     currPath: string;
@@ -31,47 +78,12 @@ export const Navbar = ({ currPath }: NavbarProps) => {
                 <Logo />
 
                 <Stack direction="row" color="gray.600">
-                    <Link href="/login" passHref>
-                        <Button
-                            size="sm"
-                            colorScheme="gray"
-                            variant="ghost"
-                            as="a"
-                            bg={currPath === "/login" ? "gray.100" : ""}
-                        >
-                            <Heading as="p" fontSize="sm">
-                                login
-                            </Heading>
-                        </Button>
-                    </Link>
-
-                    <Link href="/signup" passHref>
-                        <Button
-                            size="sm"
-                            bg={
-                                currPath === "/signup" ? "gray.100" : "gray.500"
-                            }
-                            variant="solid"
-                            as="a"
-                            _hover={{
-                                color:
-                                    currPath === "/signup"
-                                        ? "white"
-                                        : "gray.600",
-                                bg:
-                                    currPath === "/signup"
-                                        ? "gray.500"
-                                        : "gray.100",
-                            }}
-                            color={
-                                currPath === "/signup" ? "gray.600" : "white"
-                            }
-                        >
-                            <Heading as="p" fontSize="sm">
-                                sign up
-                            </Heading>
-                        </Button>
-                    </Link>
+                    <GhostLink currPath={currPath} href="/login">
+                        Login
+                    </GhostLink>
+                    <SolidLink currPath={currPath} href="/signup">
+                        Sign up
+                    </SolidLink>
                 </Stack>
             </Box>
         </Container>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
+import { useAuth } from "../store/AuthProvider";
 
 const Home = () => {
+    const { currentUser } = useAuth();
     return (
         <Box>
+            <pre>here: {JSON.stringify(currentUser)}</pre>
             <Heading>news.</Heading>
             <Text>Hello World</Text>
         </Box>
