595496fbfb1dd3e1321d4cc9bdb48aac45cf236a
Joshua-Chu <jpchu@up.edu.ph>
2022-02-19 21:21:07 +0800

    feat: add create news functionality

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,6 +1,7 @@
 import {
     Box,
     Button,
+    CloseButton,
     Container,
     Flex,
     FormControl,
@@ -9,8 +10,11 @@ import {
     Text,
 } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import Image from "next/image";
+import React, { useState } from "react";
 import { SectionTitle } from "../component/SectionTitle";
+import { supabase } from "../lib/supabase/supabaseClient";
+import { useAuth } from "../store/AuthProvider";
 
 const QuillNoSSRWrapper = dynamic(import("react-quill"), {
     ssr: false,
@@ -18,18 +22,83 @@ const QuillNoSSRWrapper = dynamic(import("react-quill"), {
 });
 
 const CreateNews = () => {
+    const { currentUser } = useAuth();
+
+    const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
+    const [imageSrc, setImageSrc] = useState<string | ArrayBuffer | null>("");
+    const [imageFile, setImageFile] = useState<File | null>(null);
+
+    const imageUploadHandler = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+
+        const target = e.target as HTMLInputElement;
+        const reader = new FileReader();
+
+        reader.onload = onLoadEvent => {
+            setImageSrc(onLoadEvent.target && onLoadEvent.target.result);
+        };
+
+        reader.readAsDataURL((target.files && target.files[0]) as Blob);
+
+        setImageFile(target.files && target.files[0]);
+    };
+
+    const onCreateNewsHandler = async (e: React.SyntheticEvent) => {
+        e.preventDefault();
+
+        // Extract Image
+        const formData = new FormData();
+        if (imageFile) {
+            formData.append("file", imageFile);
+        }
+        formData.append("upload_preset", "news-app");
+
+        // // Store images to Cloudinary
+        const imageData = await fetch(
+            "https://api.cloudinary.com/v1_1/dlfecpmkj/image/upload",
+            {
+                method: "POST",
+                body: formData,
+            }
+        ).then(r => r.json());
+
+        // Create Post
+
+        const { data } = await supabase.from("news").insert([
+            {
+                title,
+                content,
+                banner: imageData.secure_url,
+                author: currentUser?.id,
+            },
+        ]);
+
+        if (data) {
+            setTitle("");
+            setContent("");
+            setImageSrc("");
+            setImageFile(null);
+        }
+    };
+
     return (
         <Container>
             <SectionTitle>Create</SectionTitle>
-            <form>
+            <form onSubmit={onCreateNewsHandler}>
                 <Box maxW={{ sm: "390px" }} mx="auto">
                     <Flex direction="column" gap="4">
                         <FormControl>
                             <FormLabel htmlFor="title">
                                 <Text color="gray.600">title</Text>
                             </FormLabel>
-                            <Input id="title" type="text" size="md" />
+                            <Input
+                                id="title"
+                                type="text"
+                                size="md"
+                                value={title}
+                                onChange={e => setTitle(e.target.value)}
+                            />
                         </FormControl>
                         <FormControl>
                             <FormLabel htmlFor="title">
@@ -47,7 +116,7 @@ const CreateNews = () => {
                             />
                         </FormControl>
 
-                        {/* <Flex direction="column" gap="32px">
+                        <Flex direction="column" gap="32px" mt="32px">
                             <FormControl>
                                 <FormLabel htmlFor="profile-photo">
                                     <Text color="gray.600">profile photo</Text>
@@ -56,6 +125,7 @@ const CreateNews = () => {
                                     id="profile-photo"
                                     type="file"
                                     size="md"
+                                    onChange={imageUploadHandler}
                                 />
                             </FormControl>
                             {imageSrc && (
@@ -66,25 +136,18 @@ const CreateNews = () => {
                                 border={imageSrc ? "" : "1px"}
                                 w="full"
                                 h="380px"
-                                borderRadius="full"
                                 position="relative"
-                                sx={{
-                                    ".image-holder": {
-                                        borderRadius: "full",
-                                    },
-                                }}
                             >
                                 {imageSrc && (
                                     <>
                                         <Image
-                                            className="image-holder"
                                             src={imageSrc as unknown as string}
                                             layout="fill"
                                         />
                                     </>
                                 )}
                             </Box>
-                        </Flex> */}
+                        </Flex>
 
                         <Button
                             alignSelf="center"
