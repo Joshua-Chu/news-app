a81209dd672f3f403ef33f04202758d092260edc
Joshua-Chu <jpchu@up.edu.ph>
2022-02-20 23:49:12 +0800

    feat: add tooltip to news card

--- component/NewsCard/NewsCard.tsx
+++ component/NewsCard/NewsCard.tsx
@@ -4,6 +4,7 @@ import {
     Heading,
     Stack,
     Text,
+    Tooltip,
     useColorModeValue,
 } from "@chakra-ui/react";
 import Image from "next/image";
@@ -46,83 +47,85 @@ export const NewsCard = ({
     const { text } = useReadingTime(content);
     return (
         <>
-            <Box
-                onClick={() => router.push(`/news/${id}`)}
-                mx="auto"
-                key={id}
-                maxW={{ base: "350px", lg: "300px" }}
-                w="full"
-                bg={useColorModeValue("white", "gray.900")}
-                boxShadow="lg"
-                rounded="md"
-                p={6}
-                overflow="hidden"
-                cursor="pointer"
-                sx={{
-                    "&:hover": {
-                        ".delete-btn": {
-                            visibility: "visible",
-                            transition: "all 1s ease",
-                        },
-                    },
-                }}
-            >
+            <Tooltip label={title} placement="top">
                 <Box
-                    h="200px"
-                    bg="gray.100"
-                    mt={-6}
-                    mx={-6}
-                    mb={6}
-                    pos="relative"
-                >
-                    <Image src={banner} layout="fill" />
-                    {route === "/profile" &&
-                        currentUser &&
-                        author.id === currentUser.id && (
-                            <>
-                                <DeleteButton title={title} id={id} />
-                                <EditButton title={title} id={id} />
-                            </>
-                        )}
-                </Box>
-                <Stack>
-                    <Heading
-                        color={useColorModeValue("gray.700", "white")}
-                        fontSize="2xl"
-                    >
-                        {title}
-                    </Heading>
-                    <Box
-                        color="gray.500"
-                        dangerouslySetInnerHTML={{
-                            __html: content
-                                .replace(/<a/g, "<span")
-                                .replace(/a>/g, "span>"),
-                        }}
-                        noOfLines={3}
-                        sx={{
-                            a: {
-                                outline: "none",
+                    onClick={() => router.push(`/news/${id}`)}
+                    mx="auto"
+                    key={id}
+                    maxW={{ base: "350px", lg: "300px" }}
+                    w="full"
+                    bg={useColorModeValue("white", "gray.900")}
+                    boxShadow="lg"
+                    rounded="md"
+                    p={6}
+                    overflow="hidden"
+                    cursor="pointer"
+                    sx={{
+                        "&:hover": {
+                            ".delete-btn": {
+                                visibility: "visible",
+                                transition: "all 1s ease",
                             },
-                        }}
-                    />
-                </Stack>
-                <Stack mt={6} direction="row" spacing={4} align="center">
-                    <Avatar
-                        src={author.profile_photo}
-                        name="Author"
-                        bg="unset"
-                    />
-                    <Stack direction="column" spacing={0} fontSize="sm">
-                        <Text fontWeight={600}>
-                            @{author.email.split("@")[0]}
-                        </Text>
-                        <Text color="gray.500">
-                            {computedDate}· {text}
-                        </Text>
+                        },
+                    }}
+                >
+                    <Box
+                        h="200px"
+                        bg="gray.100"
+                        mt={-6}
+                        mx={-6}
+                        mb={6}
+                        pos="relative"
+                    >
+                        <Image src={banner} layout="fill" />
+                        {route === "/profile" &&
+                            currentUser &&
+                            author.id === currentUser.id && (
+                                <>
+                                    <DeleteButton title={title} id={id} />
+                                    <EditButton title={title} id={id} />
+                                </>
+                            )}
+                    </Box>
+                    <Stack>
+                        <Heading
+                            color={useColorModeValue("gray.700", "white")}
+                            fontSize="2xl"
+                        >
+                            {title}
+                        </Heading>
+                        <Box
+                            color="gray.500"
+                            dangerouslySetInnerHTML={{
+                                __html: content
+                                    .replace(/<a/g, "<span")
+                                    .replace(/a>/g, "span>"),
+                            }}
+                            noOfLines={3}
+                            sx={{
+                                a: {
+                                    outline: "none",
+                                },
+                            }}
+                        />
                     </Stack>
-                </Stack>
-            </Box>
+                    <Stack mt={6} direction="row" spacing={4} align="center">
+                        <Avatar
+                            src={author.profile_photo}
+                            name="Author"
+                            bg="unset"
+                        />
+                        <Stack direction="column" spacing={0} fontSize="sm">
+                            <Text fontWeight={600}>
+                                @{author.email.split("@")[0]}
+                            </Text>
+                            <Text color="gray.500">
+                                {computedDate}· {text}
+                            </Text>
+                        </Stack>
+                    </Stack>
+                </Box>
+            </Tooltip>
         </>
     );
 };
