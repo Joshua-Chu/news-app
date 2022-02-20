c831e6375853a7a1ee31de4d6be508c1d01ee9fc
Joshua-Chu <jpchu@up.edu.ph>
2022-02-20 23:34:47 +0800

    feat: add 404

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import Image from "next/image";
+
+export default function Custom404() {
+    return (
+        <Box position="relative" h="600px" w="80%" mx="auto">
+            <Image
+                src="https://res.cloudinary.com/dlfecpmkj/image/upload/v1645371137/kn1x2k0iuyjvc3xroxva.png"
+                layout="fill"
+            />
+        </Box>
+    );
+}
